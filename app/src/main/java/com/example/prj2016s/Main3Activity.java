442cdd513ec695cd5fb36a9927b8df6e3fdafa7d
NamHyeongKeol <grey_pastor@naver.com>
2016-06-12 00:20:48 +0900

    merge Main3Activity with MediaBuffer

--- app/src/main/java/com/example/prj2016s/Main3Activity.java
+++ app/src/main/java/com/example/prj2016s/Main3Activity.java
@@ -2,6 +2,8 @@ package com.example.prj2016s;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -9,13 +11,20 @@ import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.SurfaceHolder;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import java.io.File;
 
-public class Main3Activity extends Activity implements SurfaceHolder.Callback {
+public class Main3Activity extends Activity implements SurfaceHolder.Callback, Camera.PreviewCallback {
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        textView.setText("DATA: " + data.toString());
+        textView.setHighlightColor(Color.BLACK);
+        textView.setTextColor(Color.WHITE);
+        textView.setShadowLayer(2.0f, 1.0f, 1.0f, Color.BLACK);
+    }
+
     @Override
     protected void onPause() {
         // TODO Auto-generated method stub
@@ -44,41 +53,91 @@ public class Main3Activity extends Activity implements SurfaceHolder.Callback {
     // 녹화 시간 - 10초
     private static final int RECORDING_TIME = 10000;
 
-    // 카메라 프리뷰를 설정한다
-    private void setCameraPreview(SurfaceHolder holder){
-        try {
-            // 카메라 객체를 만든다
-            mCamera = Camera.open();
-            // 카메라 객체의 파라메터를 얻고 로테이션을 90도 꺽는다
-//            Camera.Parameters parameters = mCamera.getParameters();
-//            parameters.setRotation(90);
-//            Log.e("CAM TEST", "asdfasdf1111" + parameters.toString());
-//            mCamera.setParameters(parameters);
-//            Log.e("CAM TEST", "asdfasdf2222" + parameters.toString());
-            // 프리뷰 디스플레이를 담당한 서피스 홀더를 설정한다
-            mCamera.setPreviewDisplay(holder);
-            // 프리뷰 콜백을 설정한다 - 프레임 설정이 가능하다,
-            Log.e("CAM TEST", "asdfasdf3333");
-  /* mCamera.setPreviewCallback(new PreviewCallback() {
+    SurfaceHolder mHolder = null;
+    TextView textView = null;
+
+    /** Called when the activity is first created. */
     @Override
-    public void onPreviewFrame(byte[] data, Camera camera) {
-     // TODO Auto-generated method stub
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        // 세로화면 고정으로 처리한다
+        //SCREEN_ORIENTATION_LANDSCAPE - 가로화면 고정
+        //SCREEN_ORIENTATION_PORTRAIT - 세로화면 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // 프리뷰를 설정한다
+        // setPreview();
+
+        // 버튼을 설정한다
+//        setButtons();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.media_buffer);
+
+        mVideoView = (VideoView)findViewById(R.id.videoView);
+        textView = (TextView)findViewById(R.id.textView);
+
+        mCamera = Camera.open();
+        Camera.Parameters params = mCamera.getParameters();
+        params.setPreviewFpsRange(15000, 30000);
+        params.setPreviewSize(1280, 720);
+        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+//        params.setPictureFormat(ImageFormat.NV21);
+        params.setPreviewFormat(ImageFormat.NV21);
+        params.setPreviewFrameRate(30);
+        mCamera.setDisplayOrientation(90);
+        mCamera.setParameters(params);
+
+        mHolder = mVideoView.getHolder();
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mHolder.addCallback(this);
     }
-   });
-   */
-        } catch (Exception e) {
-            Log.e("CAM TEST","asdfasdfParams???!!!");
-            e.printStackTrace();
-            // TODO: handle exception
-        }
-    }
+
+    // 카메라 프리뷰를 설정한다
+//    private void setCameraPreview(SurfaceHolder holder){
+//        try {
+//            // 카메라 객체를 만든다
+//            mCamera = Camera.open();
+//            // 카메라 객체의 파라메터를 얻고 로테이션을 90도 꺽는다
+////            Camera.Parameters parameters = mCamera.getParameters();
+////            parameters.setRotation(90);
+////            Log.e("CAM TEST", "asdfasdf1111" + parameters.toString());
+////            mCamera.setParameters(parameters);
+////            Log.e("CAM TEST", "asdfasdf2222" + parameters.toString());
+//            // 프리뷰 디스플레이를 담당한 서피스 홀더를 설정한다
+//            mCamera.setPreviewDisplay(holder);
+//            // 프리뷰 콜백을 설정한다 - 프레임 설정이 가능하다,
+//            Log.e("CAM TEST", "asdfasdf3333");
+//  /* mCamera.setPreviewCallback(new PreviewCallback() {
+//    @Override
+//    public void onPreviewFrame(byte[] data, Camera camera) {
+//     // TODO Auto-generated method stub
+//    }
+//   });
+//   */
+//        } catch (Exception e) {
+//            Log.e("CAM TEST","asdfasdfParams???!!!");
+//            e.printStackTrace();
+//            // TODO: handle exception
+//        }
+//    }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // TODO Auto-generated method stub
         // 서피스가 만들어졌을 때의 대응 루틴
-        setCameraPreview(holder);
+//        setCameraPreview(holder);
+        try {
+            mCamera.unlock();
+            mCamera.reconnect();
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width,
                                int height) {
@@ -91,7 +150,8 @@ public class Main3Activity extends Activity implements SurfaceHolder.Callback {
 //            parameters.setPreviewSize(width,height);
 //            mCamera.setParameters(parameters);
             // 프리뷰 다시 시작
-            mCamera.startPreview();
+            mCamera.setPreviewCallback(this);
+//            mCamera.startPreview();
         }
         }
 
@@ -100,76 +160,75 @@ public class Main3Activity extends Activity implements SurfaceHolder.Callback {
         // TODO Auto-generated method stub
 
         //서피스 소멸시의 대응 루틴
-
-        // 프리뷰를 멈춘다
-        if (mCamera != null){
-            mCamera.stopPreview();
-            // 카메라 객체 초기화
-            mCamera = null;
-        }
+//
+//        // 프리뷰를 멈춘다
+//        if (mCamera != null){
+//            mCamera.stopPreview();
+//            // 카메라 객체 초기화
+//            mCamera = null;
+//        }
 
     }
     // 프리뷰(카메라가 찍고 있는 화상을 보여주는 화면) 설정 함수
-    private void setPreview()
-    {
-        // 1) 레이아웃의 videoView 를 멤버 변수에 매핑한다
-        mVideoView = (VideoView) findViewById(R.id.videoView);
-        // 2) surface holder 변수를 만들고 videoView로부터 인스턴스를 얻어온다
-        final SurfaceHolder holder = mVideoView.getHolder();
-        // 3)표면의 변화를 통지받을 콜백 객체를 등록한다
-        holder.addCallback(this);
-        // 4)Surface view의 유형을 설정한다, 아래 타입은 버퍼가 없이도 화면을 표시할 때 사용된다.카메라 프리뷰는 별도의 버퍼가 필요없다
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+//    private void setPreview()
+//    {
+//        // 1) 레이아웃의 videoView 를 멤버 변수에 매핑한다
+//        mVideoView = (VideoView) findViewById(R.id.videoView);
+//        // 2) surface holder 변수를 만들고 videoView로부터 인스턴스를 얻어온다
+//        final SurfaceHolder holder = mVideoView.getHolder();
+//        // 3)표면의 변화를 통지받을 콜백 객체를 등록한다
+//        holder.addCallback(this);
+//        // 4)Surface view의 유형을 설정한다, 아래 타입은 버퍼가 없이도 화면을 표시할 때 사용된다.카메라 프리뷰는 별도의 버퍼가 필요없다
+//        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+//
+//    }
 
-    }
-
-    private void setButtons()
-    {
-        // Rec Start 버튼 콜백 설정
-        Button recStart = (Button)findViewById(R.id.RecStart);
-        recStart.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Log.e("CAM TEST","REC START!!!!");
-
-                if (mVideoView.getHolder() == null)
-                {
-                    Log.e("CAM TEST","View Err!!!!");
-                }
-                beginRecording(mVideoView.getHolder());
-
-            }
-        });
-
-        // Rec Stop 버튼 콜백 설정
-        Button recStop = (Button)findViewById(R.id.RecStop);
-        recStop.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                // 레코더 객체가 존재할 경우 이를 스톱시킨다
-                if ( recorder !=null){
-                    Log.e("CAM TEST","CAMERA STOP!!!!!");
-                    recorder.stop();
-                    recorder.reset();
-                    recorder.release();
-                    recorder = null;
-                }
-                // 프리뷰가 없을 경우 다시 가동 시킨다
-                if ( mCamera == null ) {
-                    Log.e("CAM TEST","Preview Restart!!!!!");
-                    // 프리뷰 다시 설정
-                    setCameraPreview(mVideoView.getHolder());
-                    // 프리뷰 재시작
-                    mCamera.startPreview();
-                }
-
-            }
-        });
-    }
+//    private void setButtons()
+//    {
+//        // Rec Start 버튼 콜백 설정
+//        Button recStart = (Button)findViewById(R.id.RecStart);
+//        recStart.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                Log.e("CAM TEST", "REC START!!!!");
+//
+//                if (mVideoView.getHolder() == null) {
+//                    Log.e("CAM TEST", "View Err!!!!");
+//                }
+//                beginRecording(mVideoView.getHolder());
+//
+//            }
+//        });
+//
+//        // Rec Stop 버튼 콜백 설정
+//        Button recStop = (Button)findViewById(R.id.RecStop);
+//        recStop.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                // 레코더 객체가 존재할 경우 이를 스톱시킨다
+//                if ( recorder !=null){
+//                    Log.e("CAM TEST","CAMERA STOP!!!!!");
+//                    recorder.stop();
+//                    recorder.reset();
+//                    recorder.release();
+//                    recorder = null;
+//                }
+//                // 프리뷰가 없을 경우 다시 가동 시킨다
+//                if ( mCamera == null ) {
+//                    Log.e("CAM TEST","Preview Restart!!!!!");
+//                    // 프리뷰 다시 설정
+//                    setCameraPreview(mVideoView.getHolder());
+//                    // 프리뷰 재시작
+//                    mCamera.startPreview();
+//                }
+//
+//            }
+//        });
+//    }
 
     private void beginRecording(SurfaceHolder holder) {
         // 레코더 객체 초기화
@@ -242,21 +301,5 @@ public class Main3Activity extends Activity implements SurfaceHolder.Callback {
             e.printStackTrace();
         }
 
-    }
-
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
-        // 세로화면 고정으로 처리한다
-        //SCREEN_ORIENTATION_LANDSCAPE - 가로화면 고정
-        //SCREEN_ORIENTATION_PORTRAIT - 세로화면 고정
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        // 프리뷰를 설정한다
-        setPreview();
-
-        // 버튼을 설정한다
-        setButtons();
     }
 }
